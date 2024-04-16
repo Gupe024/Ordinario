3568df4394053ae9107a44caf054c35a6dd2cba5
Canul <canuladriana24@gmail.com>
2024-04-16 08:49:46 -0600

    Logica de regisro e inicio de sesión

--- app/src/main/java/com/example/jugueteriaapp/MainActivity.java
+++ app/src/main/java/com/example/jugueteriaapp/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.jugueteriaapp;
 
+import static com.example.jugueteriaapp.R.id.Registro2;
+import static com.example.jugueteriaapp.R.id.password;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +19,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText user;
+    EditText pass;
+    Button buttoniniciar;
+    Button buttonregistrarse = findViewById(R.id.Registro2);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        user = findViewById(R.id.usuario);
+        pass = findViewById(R.id.password);
+        buttoniniciar = findViewById(R.id.iniciar);
+
+        buttoniniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarSesion();
+            }
         });
+
+        buttonregistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void iniciarSesion() {
+        String usuario = user.getText().toString();
+        String password = pass.getText().toString();
+
+        if(verificarcredenciales(usuario, password)){
+            Toast.makeText(this, "Bienvenid@"+usuario+",¿Qué te podemos llevar a tú casa este día? Por favor selecciona:", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Usuario y contraseña incorrectos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean verificarcredenciales(String usuario, String password) {
+        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+
+        String UsuarioAlmacenado = sharedPreferences.getString("Usuario", "");
+        String passAlmacenado = sharedPreferences.getString("Password","");
+
+        return usuario.equals(UsuarioAlmacenado) && password.equals(passAlmacenado);
     }
 }
--- app/src/main/java/com/example/jugueteriaapp/MainActivity2.java
+++ app/src/main/java/com/example/jugueteriaapp/MainActivity2.java
@@ -1,6 +1,12 @@
 package com.example.jugueteriaapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,62 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    EditText Editnombre;
+    EditText Editusuario;
+    EditText Editpassword;
+    EditText Edittelefono;
+    EditText Editcorreo;
+    EditText Editdireccion;
+    Button Registro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Editnombre = findViewById(R.id.nombre);
+        Editusuario = findViewById(R.id.usuario);
+        Editpassword = findViewById(R.id.password);
+        Edittelefono = findViewById(R.id.telefono);
+        Editcorreo = findViewById(R.id.correo);
+        Editdireccion = findViewById(R.id.direccion);
+        Registro = findViewById(R.id.registro);
+
+        Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                registroUsuario(
+                    Editnombre.getText().toString(),
+                    Editusuario.getText().toString(),
+                    Editpassword.getText().toString(),
+                    Edittelefono.getText().toString(),
+                    Editcorreo.getText().toString(),
+                    Editdireccion.getText().toString()
+                );
+
+            }
         });
     }
+
+    private void registroUsuario(String nombre, String usuario, String password, String telefono, String correo, String direccion) {
+        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("nombre", nombre);
+        editor.putString("usuario", usuario);
+        editor.putString("password", password);
+        editor.putString("telefono", telefono);
+        editor.putString("correo", correo);
+        editor.putString("direccion", direccion);
+
+        editor.apply();
+
+        Toast.makeText(this, "¡Usuario registrado correctamente!", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+    }
+
+
 }
